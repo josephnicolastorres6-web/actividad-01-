9cec07d3be16ae1502b179951e17d89a2b2bf790
josephnicolastorres6-web <josephnicolastorres6@gmail.com>
2026-02-04 12:17:02 -0500

    Change admin user to 'nicolas' for access

--- 4.02.2026/ejercicio 5.py
+++ 4.02.2026/ejercicio 5.py
@@ -1,6 +1,7 @@
 usuario=input("ingresa tu nombre de usuario: ")
 contrraseña=input("ingresa tu contraseña: ")
-if usuario == "admin" and contrraseña == "1234":
+if usuario == "nicolas" and contrraseña == "1234":
     print("acceso concedido")
 else:
+
     print("usuario o contraseña incorrectos")
